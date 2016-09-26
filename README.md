5806e1c2726f0822a44b0d8bbf435e4e100bcae7
tsondt <tsondt@gmail.com>
2016-09-26 17:43:18 -0400

    Add .Xresources and modify README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 #My dotfiles
+`bash`,`git` and `Vundle`are needed.
+- Install Vundle:
+  `git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim`
+- Then run:
+  `/bin/bash ./setup.sh`
+
--- setup.sh
+++ setup.sh
@@ -12,11 +12,15 @@ ln -s $PWD/.tmuxline.conf $HOME/.tmuxline.conf
 [[ -f $HOME/.promptline.conf ]] && mv $HOME/.promptline.conf $HOME/.promptline.conf.bak
 [[ -L $HOME/.promptline.conf ]] && rm $HOME/.promptline.conf
 ln -s $PWD/.promptline.conf $HOME/.promptline.conf
-[[ -s "$HOME/.bashrc" ]] && sed -i.bak '/promptline\.conf/d' $HOME/.bashrc
+[[ -s "$HOME/.bashrc" ]] && sed -i.bak-promptline '/promptline\.conf/d' $HOME/.bashrc
 echo '. ~/.promptline.conf' >> $HOME/.bashrc
+[[ -s "$HOME/.bashrc" ]] && sed -i.bak-term '/export TERM=/d' $HOME/.bashrc
 echo 'export TERM="xterm-256color"' >> $HOME/.bashrc
 [[ -f "$HOME/base16-paraiso.dark.sh" ]] && mv $HOME/base16-paraiso.dark.sh $HOME/base16-paraiso.dark.sh.bak
 [[ -L "$HOME/base16-paraiso.dark.sh" ]] && rm $HOME/base16-paraiso.dark.sh
 ln -s $PWD/base16-paraiso.dark.sh $HOME/base16-paraiso.dark.sh
-[[ -s "$HOME/.bashrc" ]] && sed -i.bak '/base16-paraiso\.dark\.sh/d' $HOME/.bashrc
+[[ -s "$HOME/.bashrc" ]] && sed -i.bak-base16 '/base16-paraiso\.dark\.sh/d' $HOME/.bashrc
 echo '. ~/base16-paraiso.dark.sh' >> $HOME/.bashrc
+[[ -f "$HOME/.Xresources" ]] && mv $HOME/.Xresources $HOME/.Xresources.bak
+[[ -L "$HOME/.Xresources" ]] && rm $HOME/.Xresources
+ln -s $PWD/.Xresources $HOME/.Xresources
