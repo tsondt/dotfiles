f4ce3b0a54104378ac1488d2a92ed55cbed96ce3
tsondt <tsondt@gmail.com>
2016-09-26 17:51:34 -0400

    Fix symbolic link detection, add true-color-test.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
-[[ -f $HOME/.vimrc ]] && mv $HOME/.vimrc $HOME/.vimrc.bak
 [[ -L $HOME/.vimrc ]] && rm $HOME/.vimrc
+[[ -f $HOME/.vimrc ]] && mv $HOME/.vimrc $HOME/.vimrc.bak
 ln -s $PWD/.vimrc $HOME/.vimrc
 vim +PluginInstall +qall
-[[ -f $HOME/.tmux.conf ]] && mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
 [[ -L $HOME/.tmux.conf ]] && rm $HOME/.tmux.conf
+[[ -f $HOME/.tmux.conf ]] && mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
 ln -s $PWD/.tmux.conf $HOME/.tmux.conf
-[[ -f $HOME/.tmuxline.conf ]] && mv $HOME/.tmuxline.conf $HOME/.tmuxline.conf.bak
 [[ -L $HOME/.tmuxline.conf ]] && rm $HOME/.tmuxline.conf
+[[ -f $HOME/.tmuxline.conf ]] && mv $HOME/.tmuxline.conf $HOME/.tmuxline.conf.bak
 ln -s $PWD/.tmuxline.conf $HOME/.tmuxline.conf
-[[ -f $HOME/.promptline.conf ]] && mv $HOME/.promptline.conf $HOME/.promptline.conf.bak
 [[ -L $HOME/.promptline.conf ]] && rm $HOME/.promptline.conf
+[[ -f $HOME/.promptline.conf ]] && mv $HOME/.promptline.conf $HOME/.promptline.conf.bak
 ln -s $PWD/.promptline.conf $HOME/.promptline.conf
 [[ -s "$HOME/.bashrc" ]] && sed -i.bak-promptline '/promptline\.conf/d' $HOME/.bashrc
 echo '. ~/.promptline.conf' >> $HOME/.bashrc
 [[ -s "$HOME/.bashrc" ]] && sed -i.bak-term '/export TERM=/d' $HOME/.bashrc
 echo 'export TERM="xterm-256color"' >> $HOME/.bashrc
-[[ -f "$HOME/base16-paraiso.dark.sh" ]] && mv $HOME/base16-paraiso.dark.sh $HOME/base16-paraiso.dark.sh.bak
 [[ -L "$HOME/base16-paraiso.dark.sh" ]] && rm $HOME/base16-paraiso.dark.sh
+[[ -f "$HOME/base16-paraiso.dark.sh" ]] && mv $HOME/base16-paraiso.dark.sh $HOME/base16-paraiso.dark.sh.bak
 ln -s $PWD/base16-paraiso.dark.sh $HOME/base16-paraiso.dark.sh
 [[ -s "$HOME/.bashrc" ]] && sed -i.bak-base16 '/base16-paraiso\.dark\.sh/d' $HOME/.bashrc
 echo '. ~/base16-paraiso.dark.sh' >> $HOME/.bashrc
-[[ -f "$HOME/.Xresources" ]] && mv $HOME/.Xresources $HOME/.Xresources.bak
 [[ -L "$HOME/.Xresources" ]] && rm $HOME/.Xresources
+[[ -f "$HOME/.Xresources" ]] && mv $HOME/.Xresources $HOME/.Xresources.bak
 ln -s $PWD/.Xresources $HOME/.Xresources
