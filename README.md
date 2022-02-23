4c5f76a4ffb7a96256b395555a54dcb20b779e31
Son Dinh <tsondt@gmail.com>
2022-02-23 18:50:38 -0500

    Some fixes

--- README.md
+++ README.md
@@ -7,18 +7,13 @@
 - `vim` (or `neovim`)
 
 ## Setup
-- Install `git`, `tmux`, `vim` (Ubuntu)
+- Install `git`, `tmux`, `vim` & `neovim`
   ```
-  sudo apt update && sudo apt purge -y vim-tiny && sudo apt install -y git tmux vim-nox
-  ```
-- Install `neovim` (Ubuntu) (https://github.com/neovim/neovim/wiki/Installing-Neovim#ubuntu)
-  ```
-  sudo add-apt-repository ppa:neovim-ppa/stable
-  sudo apt update && sudo apt install -y neovim
+  sudo apt update && sudo apt purge -y vim-tiny && sudo apt install -y git tmux vim-nox neovim
   ```
 
 - Install `vim-plug` (https://github.com/junegunn/vim-plug#installation)
-  
+
   Vim
   ```
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -35,7 +30,7 @@
   ./install.sh
   ```
 
-- One-liner on new Lubuntu installation:
+- One-liner on new Ubuntu installation:
   ```
-  sudo apt update && sudo apt purge -y vim-tiny && sudo apt install -y git tmux vim-nox software-properties-common && sudo add-apt-repository ppa:neovim-ppa/stable && sudo apt update && sudo apt install -y neovim && curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && ./install.sh
+  sudo apt update && sudo apt purge -y vim-tiny && sudo apt install -y git tmux vim-nox neovim software-properties-common && curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && ./install.sh
   ```
--- install.sh
+++ install.sh
@@ -1,41 +1,50 @@
 #!/usr/bin/env bash
-[[ -L $HOME/.vimrc ]] && rm $HOME/.vimrc
-[[ -f $HOME/.vimrc ]] && mv $HOME/.vimrc $HOME/.vimrc.bak
-ln -s $PWD/.vimrc $HOME/.vimrc
-vim +PlugInstall +qall
-[[ -L $HOME/.config/nvim/init.vim ]] && rm $HOME/.config/nvim/init.vim
-[[ -f $HOME/.config/nvim/init.vim ]] && mv $HOME/.config/nvim/init.vim $HOME/.config/nvim/init.vim.bak
-ln -s $PWD/init.vim $HOME/.config/nvim/init.vim
-nvim +PlugInstall +qall
-[[ -L $HOME/.tmux.conf ]] && rm $HOME/.tmux.conf
-[[ -f $HOME/.tmux.conf ]] && mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
-ln -s $PWD/.tmux.conf $HOME/.tmux.conf
-[[ -L $HOME/.tmux-default-session ]] && rm $HOME/.tmux-default-session
-[[ -f $HOME/.tmux-default-session ]] && mv $HOME/.tmux-default-session $HOME/.tmux-default-session.bak
-ln -s $PWD/.tmux-default-session $HOME/.tmux-default-session
-[[ -L $HOME/.tmuxline.conf ]] && rm $HOME/.tmuxline.conf
-[[ -f $HOME/.tmuxline.conf ]] && mv $HOME/.tmuxline.conf $HOME/.tmuxline.conf.bak
-ln -s $PWD/.tmuxline.conf $HOME/.tmuxline.conf
-[[ -L $HOME/.promptline.conf ]] && rm $HOME/.promptline.conf
-[[ -f $HOME/.promptline.conf ]] && mv $HOME/.promptline.conf $HOME/.promptline.conf.bak
-ln -s $PWD/.promptline.conf $HOME/.promptline.conf
-[[ -s "$HOME/.bashrc" ]] && sed -i.bak-promptline '/promptline\.conf/d' $HOME/.bashrc
+[[ -L "$HOME/.vimrc" ]] && rm "$HOME/.vimrc"
+[[ -f "$HOME/.vimrc" ]] && mv "$HOME/.vimrc" "$HOME/.vimrc.bak"
+ln -s "$PWD/.vimrc" "$HOME/.vimrc"
+vim +PlugInstall +PlugUpdate +qall
+
+[[ -L "$HOME/.config/nvim/init.vim" ]] && rm "$HOME/.config/nvim/init.vim"
+[[ -f "$HOME/.config/nvim/init.vim" ]] && mv "$HOME/.config/nvim/init.vim" "$HOME/.config/nvim/init.vim.bak"
+[[ -d "$HOME/.config/nvim" ]] || mkdir -p "$HOME/.config/nvim"
+ln -s "$PWD/init.vim" "$HOME/.config/nvim/init.vim"
+nvim +PlugInstall +PlugUpdate +qall
+
+[[ -L "$HOME/.tmux.conf" ]] && rm "$HOME/.tmux.conf"
+[[ -f "$HOME/.tmux.conf" ]] && mv "$HOME/.tmux.conf" "$HOME/.tmux.conf.bak"
+ln -s "$PWD/.tmux.conf" "$HOME/.tmux.conf"
+
+[[ -L ""$HOME/.tmux-default-session"" ]] && rm "$HOME/.tmux-default-session"
+[[ -f "$HOME/.tmux-default-session" ]] && mv "$HOME/.tmux-default-session" "$HOME/.tmux-default-session.bak"
+ln -s "$PWD/.tmux-default-session" "$HOME/.tmux-default-session"
+
+[[ -L "$HOME/.tmuxline.conf" ]] && rm "$HOME/.tmuxline.conf"
+[[ -f "$HOME/.tmuxline.conf" ]] && mv "$HOME/.tmuxline.conf" "$HOME/.tmuxline.conf.bak"
+ln -s "$PWD/.tmuxline.conf" "$HOME/.tmuxline.conf"
+
+[[ -L "$HOME/.Xresources" ]] && rm "$HOME/.Xresources"
+[[ -f "$HOME/.Xresources" ]] && mv "$HOME/.Xresources" "$HOME/.Xresources.bak"
+ln -s "$PWD/.Xresources" "$HOME/.Xresources"
+
+[[ -L "$HOME/.dircolors" ]] && rm "$HOME/.dircolors"
+[[ -f "$HOME/.dircolors" ]] && mv "$HOME/.dircolors" "$HOME/.dircolors.bak"
+ln -s "$PWD/.dircolors" "$HOME/.dircolors"
+
+[[ -L "$HOME/.bash_aliases" ]] && rm "$HOME/.bash_aliases"
+[[ -f "$HOME/.bash_aliases" ]] && mv "$HOME/.bash_aliases" "$HOME/.bash_aliases.bak"
+ln -s "$PWD/.bash_aliases" "$HOME/.bash_aliases"
+
+[[ -s "$HOME/.bashrc" ]] && sed -i.bak-term '/export TERM=/d' "$HOME/.bashrc" && rm "$HOME/.bashrc.bak-term"
+echo 'export TERM="xterm-256color"' >> "$HOME/.bashrc"
+
+[[ -L "$HOME/.promptline.conf" ]] && rm "$HOME/.promptline.conf"
+[[ -f "$HOME/.promptline.conf" ]] && mv "$HOME/.promptline.conf" "$HOME/.promptline.conf.bak"
+ln -s "$PWD/.promptline.conf" "$HOME/.promptline.conf"
+[[ -s "$HOME/.bashrc" ]] && sed -i.bak-promptline '/promptline\.conf/d' "$HOME/.bashrc"  && rm "$HOME/.bashrc.bak-promptline"
 echo '. ~/.promptline.conf' >> $HOME/.bashrc
-[[ -s "$HOME/.bashrc" ]] && sed -i.bak-term '/export TERM=/d' $HOME/.bashrc
-echo 'export TERM="xterm-256color"' >> $HOME/.bashrc
-[[ -L "$HOME/base16-paraiso.dark.sh" ]] && rm $HOME/base16-paraiso.dark.sh
-[[ -f "$HOME/base16-paraiso.dark.sh" ]] && mv $HOME/base16-paraiso.dark.sh $HOME/base16-paraiso.dark.sh.bak
-ln -s $PWD/base16-paraiso.dark.sh $HOME/base16-paraiso.dark.sh
-[[ -s "$HOME/.bashrc" ]] && sed -i.bak-base16 '/base16-paraiso\.dark\.sh/d' $HOME/.bashrc
+
+[[ -L "$HOME/base16-paraiso.dark.sh" ]] && rm "$HOME/base16-paraiso.dark.sh"
+[[ -f "$HOME/base16-paraiso.dark.sh" ]] && mv "$HOME/base16-paraiso.dark.sh" "$HOME/base16-paraiso.dark.sh.bak"
+ln -s "$PWD/base16-paraiso.dark.sh" "$HOME/base16-paraiso.dark.sh"
+[[ -s "$HOME/.bashrc" ]] && sed -i.bak-base16 '/base16-paraiso\.dark\.sh/d' "$HOME/.bashrc" && rm "$HOME/.bashrc.bak-base16"
 echo '. ~/base16-paraiso.dark.sh' >> $HOME/.bashrc
-[[ -L "$HOME/.Xresources" ]] && rm $HOME/.Xresources
-[[ -f "$HOME/.Xresources" ]] && mv $HOME/.Xresources $HOME/.Xresources.bak
-ln -s $PWD/.Xresources $HOME/.Xresources
-[[ -L "$HOME/.dircolors" ]] && rm $HOME/.dircolors
-[[ -f "$HOME/.dircolors" ]] && mv $HOME/.dircolors $HOME/.dircolors.bak
-ln -s $PWD/.dircolors $HOME/.dircolors
-[[ -L "$HOME/.bash_aliases" ]] && rm $HOME/.bash_aliases
-[[ -f "$HOME/.bash_aliases" ]] && mv $HOME/.bash_aliases $HOME/.bash_aliases.bak
-ln -s $PWD/.bash_aliases $HOME/.bash_aliases
-[[ -s "$HOME/.bashrc" ]] && sed -i '/xset r rate 250 100/d' $HOME/.bashrc
-echo 'xset r rate 250 100' >> $HOME/.bashrc
